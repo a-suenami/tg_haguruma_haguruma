2c95f036a1543090811c04de714ee9d24be2004e
Akira Suenami <a.suenami@twogate.com>
2025-08-14 15:18:04 +0700

    Fix docker-compose-run

--- env.sh
+++ env.sh
@@ -22,8 +22,8 @@ if command -v docker &> /dev/null; then
   alias app="rm -f tmp/pids/server.pid && up app"
   alias rails="bundle exec rails"
   alias rake="bundle exec rake"
-  alias rspec="docker-compose run -e RAILS_ENV=test --rm app rspec"
-  alias rspec_parallel="docker-compose run -e RAILS_ENV=test --rm app bash -c 'CORES=${PARALLEL_JOBS:-$( (command -v nproc >/dev/null && nproc) || getconf _NPROCESSORS_ONLN || sysctl -n hw.ncpu || echo 4 )}; bundle exec rake parallel:create parallel:prepare && bundle exec parallel_test --type rspec -n $CORES'"
+  alias rspec="docker-compose-run -e RAILS_ENV=test --rm app rspec"
+  alias rspec_parallel="docker-compose-run -e RAILS_ENV=test --rm app bash -c 'CORES=${PARALLEL_JOBS:-$( (command -v nproc >/dev/null && nproc) || getconf _NPROCESSORS_ONLN || sysctl -n hw.ncpu || echo 4 )}; bundle exec rake parallel:create parallel:prepare && bundle exec parallel_test --type rspec -n $CORES'"
   alias rubocop="bundle exec rubocop -DES --cache true"
   alias lint="bundle exec rubocop -a"
   alias rubocop_show_class="bundle exec rubocop -D"
@@ -34,9 +34,9 @@ if command -v docker &> /dev/null; then
   # Helper function to use exec if container is running, otherwise use run
   docker-compose-run() {
     if docker compose -p $project_name ps app 2>/dev/null | grep -q "Up\|running"; then
-      docker-compose exec "$@"
+      docker compose -p $project_name exec "$@"
     else
-      docker-compose run --rm "$@"
+      docker compose -p $project_name run --rm "$@"
     fi
   }
 
