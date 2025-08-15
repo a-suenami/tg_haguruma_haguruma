147ae41f8c709e5a5092c11759138091de173213
Akira Suenami <a.suenami@twogate.com>
2025-08-15 13:38:19 +0700

    Fix rubocop

--- app/controllers/admin_area/categories_controller.rb
+++ app/controllers/admin_area/categories_controller.rb
@@ -1,4 +1,10 @@
+# typed: true
+# frozen_string_literal: true
+
 class AdminArea::CategoriesController < AdminArea::ApplicationController
+  extend T::Sig
+
+  sig { void }
   def index
   end
 end
--- app/controllers/admin_area/media_controller.rb
+++ app/controllers/admin_area/media_controller.rb
@@ -1,4 +1,10 @@
+# typed: true
+# frozen_string_literal: true
+
 class AdminArea::MediaController < AdminArea::ApplicationController
+  extend T::Sig
+
+  sig { void }
   def index
   end
 end
