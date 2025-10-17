db16eee7b0d71c807e12dd54e45a63aea4f32d20
Akira Suenami <a.suenami@twogate.com>
2025-10-17 11:38:48 +0900

    add models for admins / rulers.

---
+++ app/models/admin/auth0_account.rb
@@ -0,0 +1,11 @@
+# typed: strict
+
+class Admin::Auth0Account < ApplicationRecord
+  extend T::Sig
+
+  belongs_to :admin
+  belongs_to :auth0_account, class_name: '::Auth0Account'
+  belongs_to :tenant
+
+  validates :admin_id, uniqueness: { scope: [:auth0_account_id, :tenant_id] }
+end
