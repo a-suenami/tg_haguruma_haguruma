c4db74dcabf2230ef67f58008157646146056e12
Akira Suenami <a.suenami@twogate.com>
2025-10-17 09:54:16 +0900

    Fix rubocop, again.

--- app/models/content_entry.rb
+++ app/models/content_entry.rb
@@ -1,7 +1,7 @@
 # typed: false
 
 class ContentEntry < ApplicationRecord
-  has_many :content_tags, dependent: :destroy, foreign_key: :content_id
+  has_many :content_tags, dependent: :destroy, foreign_key: :content_id, inverse_of: :content_entry
 
   validates :tenant_id, presence: true
   validates :content_type_id, presence: true
